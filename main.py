97f959b5705b544ca4d60051d54d24c6282c90f0
Omkar <ochavhan02@gmail.com>
2026-09-18 13:23:28 +0530

    Update

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from Routes.studentroutes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app  = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
