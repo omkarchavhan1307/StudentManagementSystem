b96a66612e3f8a64134d08f76105af4d425d3f57
Omkar <ochavhan02@gmail.com>
2026-09-18 15:25:54 +0530

    Update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app  = FastAPI()
 
 origin  = ["http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://rainbow-beignet-124f09.netlify.app/",
+        "https://rainbow-beignet-124f09.netlify.app",
         ]
 
 app.add_middleware(
